f5385b3a3e8e28f1da22ac1cc982e0f9a2d7da95
Mihail Maksimov <mikle24@yandex.ru>
2019-09-12 16:16:09 +0300

    datetime is between start datetime and end datetime

--- src/ISO8601DateTime.php
+++ src/ISO8601DateTime.php
@@ -16,4 +16,36 @@ abstract class ISO8601DateTime
     {
         return new PHPDateTime($this->value()) == new PHPDateTime($dateTime->value());
     }
+
+    public function isBetween(ISO8601DateTime $dateFrom, ISO8601DateTime $dateTo): bool
+    {
+        return
+            $this->greaterOrEquals($dateFrom)
+            &&
+            $this->lessOrEquals($dateTo);
+    }
+
+    private function greaterOrEquals(ISO8601DateTime $dateTime): bool
+    {
+        return
+            new PHPDateTime(
+                $this->value()
+            )
+            >=
+            new PHPDateTime(
+                $dateTime->value()
+            );
+    }
+
+    private function lessOrEquals(ISO8601DateTime $dateTime): bool
+    {
+        return
+            new PHPDateTime(
+                $this->value()
+            )
+            <=
+            new PHPDateTime(
+                $dateTime->value()
+            );
+    }
 }
