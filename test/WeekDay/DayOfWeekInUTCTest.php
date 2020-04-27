854d5780e28654e635d82b161631cec96676b989
vadim <vadim@localhost.localdomain>
2020-04-27 11:38:27 +0300

    * refactored a bit

--- test/WeekDay/DayOfWeekInUTCTest.php
+++ test/WeekDay/DayOfWeekInUTCTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Meringue\Tests\FormattedDateTime;
+namespace Meringue\Tests\WeekDay;
 
 use Meringue\WeekDay\Tuesday;
 use PHPUnit\Framework\TestCase;
--- test/WeekDay/LocalDayOfWeekTest.php
+++ test/WeekDay/LocalDayOfWeekTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Meringue\Tests\FormattedDateTime;
+namespace Meringue\Tests\WeekDay;
 
 use Meringue\WeekDay\LocalDayOfWeek;
 use PHPUnit\Framework\TestCase;
