4ad69427e56a054d656dc0bb220d7b579abaa2b2
vadim <vadim@localhost.localdomain>
2020-12-03 19:33:41 +0300

    Type hinting fixed

--- src/Schedule.php
+++ src/Schedule.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Meringue;
 
-use Meringue\Schedule\TimePeriod\DefaultTimePeriod;
+use Meringue\Schedule\TimePeriod;
 
 interface Schedule
 {
     public function isHit(ISO8601DateTime $dateTime): bool;
 
     /**
-     * @return DefaultTimePeriod[]
+     * @return TimePeriod[]
      */
     public function for(ISO8601DateTime $dateTime): array;
 }
