14f28cc456616dba077535614f59f737111769b4
vadim <vadim@localhost.localdomain>
2020-12-08 13:03:26 +0300

    Magadan timezone added

---
+++ src/ISO8601DateTime/PhpSpecificTimeZone/Magadan.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Meringue\ISO8601DateTime\PhpSpecificTimeZone;
+
+use Meringue\ISO8601DateTime\PhpSpecificTimeZone;
+
+class Magadan implements PhpSpecificTimeZone
+{
+    public function value(): string
+    {
+        return 'Asia/Magadan';
+    }
+}
